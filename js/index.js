38ba7db417fcb4b2735e12fba26373f08cfb84a9
maitan <mtan5995@gmail.com>
2023-01-21 09:47:31 +0700

    baitap

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// bài tập 1 tính tiền lương nahnh viên
+//input
+var luongNgay =100;
+var ngayLam = 28;
+var tienLuong =0;
+ tienLuong = ngayLam * luongNgay;
+ console.log(tienLuong);
+
+//  bài 2: Tính giá trị trung bình 
+var trungBinh = 0;
+var a = 5;
+var b = 6;
+var c = 7;
+var d = 8;
+var e = 9;
+trungBinh =(a +b +c +d +e)/5;
+console.log(trungBinh);
+// bai 3: Đổi tiền 
+var giaVnd = 23.500;
+var doCanDoi = 2;
+ var thanhTien = 0;
+ thanhTien = doCanDoi * giaVnd;
+ console.log(thanhTien);
+
+ //bai 4: tinh diện tích chu vi hình chữ nhật
+ var chieuDai = 7;
+ var chieuRong = 10;
+ var ChuVi = (chieuDai + chieuRong)*2;
+ var dienTich = chieuDai * chieuRong;
+ console.log ('chu vi = '+ ChuVi + 'dien tich = '+ dienTich); 
+
+//  bài 5: tính tổng 2 ký số
+var f =34;
+var soDuHangTruc =Math.floor(f % 10 );
+var soDuHangdv = Math.floor( f / 10 );
+var tong2KySo = soDuHangTruc+ soDuHangdv;
+console.log (tong2KySo); 
